07e16c503559f09991871fc7a2cefb6c8987b726
Stefania <rinaldi.stefania09@gmail.com>
2022-12-14 20:36:40 -0300

    se agrego el metodo restar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,17 @@ export class AppComponent {
   sumar() : void{
     this.resultado = this.numero1 + this.numero2;
   }
+  restar() : void{
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+
+
+
+
+
+
+
+
+
 }
